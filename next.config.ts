c50c7c692962758038c5d0527070b5b6e26cea51
honeegee <honeegee@github.com>
2025-10-14 17:15:08 +0800

    workflows

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',
+  basePath: '/digital-pinoy-museum',
+  assetPrefix: '/digital-pinoy-museum/',
+  trailingSlash: true,
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
